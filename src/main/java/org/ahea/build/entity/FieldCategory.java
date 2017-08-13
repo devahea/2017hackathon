e8911607395fea439264635471fb8f5f57866439
김종인 <kji6252@gmail.com>
2017-08-13 20:57:33 +0900

    필드 카테고리 추가

--- src/main/java/org/ahea/build/entity/FieldCategory.java
+++ src/main/java/org/ahea/build/entity/FieldCategory.java
@@ -1,13 +1,18 @@
 package org.ahea.build.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.UUID;
 
 @Entity
 public class FieldCategory {
-    @Id @GeneratedValue(strategy = GenerationType.AUTO,generator = )
+    @Id
+    @GenericGenerator(name = "uuid-gen", strategy = "uuid2")
+    @GeneratedValue(generator = "uuid-gen")
+    @org.hibernate.annotations.Type(type="pg-uuid")
     UUID id;
 
 }
