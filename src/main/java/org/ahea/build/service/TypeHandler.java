4ba38ceeaea9af440b71ebe44492bbe271825e53
김종인 <kji6252@gmail.com>
2017-08-14 00:36:18 +0900

    service 패키지 추가

--- src/main/java/org/ahea/build/service/TypeHandler.java
+++ src/main/java/org/ahea/build/service/TypeHandler.java
@@ -1,23 +1,27 @@
 package org.ahea.build.service;
 
 import org.ahea.build.entity.FieldCategory;
+import org.ahea.build.entity.ResultData;
 import org.ahea.build.inter.BuildInterface;
 
 import java.util.List;
+import java.util.Map;
 
 
 public class TypeHandler {
 
-    BuildInterface handle(List<FieldCategory> fieldCategoryList, Integer rowNumber){
-        for (FieldCategory fieldCategory : fieldCategoryList) {
-            if(fieldCategory.getType().startsWith("repo")){
+    Map<String, List<ResultData>> handle(List<FieldCategory> fieldCategoryList, Integer rowNumber){
+        for (int i = 0; i < rowNumber; i++){
+            for (FieldCategory fieldCategory : fieldCategoryList) {
+                if(fieldCategory.getType().startsWith("repo")){
 
-            } else if("randem".equals(fieldCategory.getType())){
+                } else if("random".equals(fieldCategory.getType())){
 
-            } else if("select".equals(fieldCategory.getType())) {
+                } else if("select".equals(fieldCategory.getType())) {
 
-            } else if("date".equals(fieldCategory.getType())){
+                } else if("date".equals(fieldCategory.getType())){
 
+                }
             }
         }
 
