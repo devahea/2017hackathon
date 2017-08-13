4d19aa6d19a8579544122b20476952621c9bfe5f
김종인 <kji6252@gmail.com>
2017-08-13 21:12:27 +0900

    필드 데이타 추가

---
+++ src/main/java/org/ahea/build/repository/FieldDataRepository.java
@@ -0,0 +1,10 @@
+package org.ahea.build.repository;
+
+import org.ahea.build.entity.FieldCategory;
+import org.ahea.build.entity.FieldData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface FieldDataRepository extends JpaRepository<FieldData,Long> {
+}
