ccd4dc5807f7891f9d0cd73662fa63e56476e870
RohSunghyun <shtjdgus1090@naver.com>
2017-08-14 00:44:25 +0900

    todo 추가
    todo 추가

--- src/main/java/org/ahea/export/ExporterFactory.java
+++ src/main/java/org/ahea/export/ExporterFactory.java
@@ -7,6 +7,10 @@ public class ExporterFactory {
 
     public static Exporter newInstance(int exportType) {
 
+        //todo
+        //exportType을 Enum으로 바꾸자
+        //exportType에 따라 Exporter객체 생성
+
         return new GsonJsonExporter();
 
     }
