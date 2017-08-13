474ee652e5e81b2c7a46374d37463311fd53a2f7
mio <powalsld@naver.com>
2017-08-14 02:13:23 +0900

    test 케이스 수정

--- src/test/java/org/ahea/service/DateServiceTest.java
+++ src/test/java/org/ahea/service/DateServiceTest.java
@@ -1,8 +1,12 @@
 package org.ahea.service;
 
 import lombok.extern.apachecommons.CommonsLog;
+import org.ahea.build.entity.FieldCategory;
 import org.ahea.build.service.DateService;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by mio on 2017. 8. 14..
@@ -11,7 +15,11 @@ import org.junit.Test;
 public class DateServiceTest {
     @Test
     public void DateServiceFormatTest(){
-        DateService dateService = new DateService("yyyy/MM/dd HH:mm:ss");
-        log.debug(dateService.generateDateFormat());
+        DateService dateService = new DateService();
+        FieldCategory fieldCategory = new FieldCategory();
+        List<String> conditions = new ArrayList<>();
+        conditions.add("yyyy/MM/dd HH:mm:ss");
+        fieldCategory.setConditions(conditions);
+        log.debug(dateService.genetateData(fieldCategory));
     }
 }
