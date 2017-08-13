892236c4db213807a2ab985ffc428e28b87002fc
김종인 <kji6252@gmail.com>
2017-08-14 00:08:32 +0900

    service 패키지 추가

---
+++ src/main/java/org/ahea/build/service/TypeHandler.java
@@ -0,0 +1,23 @@
+package org.ahea.build.service;
+
+import org.ahea.build.entity.FieldCategory;
+import org.ahea.build.inter.BuildInterface;
+
+import java.util.List;
+
+
+public class TypeHandler {
+
+    BuildInterface handle(List<FieldCategory> fieldCategoryList, Integer rowNumber){
+        for (FieldCategory fieldCategory : fieldCategoryList) {
+            if(fieldCategory.getType().startsWith("repo")){
+
+            } else if("randem".equals(fieldCategory.getType())){
+
+            }
+        }
+
+        return null;
+    }
+
+}
