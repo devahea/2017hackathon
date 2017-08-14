0c7f8edd1cbb869a9b4be17b5bea71cec00b942e
mio <powalsld@naver.com>
2017-08-14 12:28:17 +0900

    factory 리펙토링 테스트 케이스 수정

--- src/test/java/org/ahea/export/json/GsonJsonExporterTest.java
+++ src/test/java/org/ahea/export/json/GsonJsonExporterTest.java
@@ -1,8 +1,11 @@
 package org.ahea.export.json;
 
 import com.google.gson.Gson;
+import lombok.extern.apachecommons.CommonsLog;
 import org.ahea.export.Exporter;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.internal.runners.statements.ExpectException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,37 +19,30 @@ import java.util.Map;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@CommonsLog
 public class GsonJsonExporterTest {
 
     private Gson gson = new Gson();
-
-    @Autowired
     Exporter gsonJsonExporter;
+    Map<String, Object> sampleData;
+    Map<String, Object> sampleData2;
+    List<Map<String, Object>> arrayListData;
 
-    @Test
-    public void singleParamDefaultOptionExport() throws Exception {
+    @Before
+    public void setup(){
+        sampleData = new HashMap<>();
+        gsonJsonExporter = new GsonJsonExporter();
 
-        //Given
-        Map<String, Object> data = new HashMap<>();
-        data.put("이름", "김민수");
-        data.put("성별", 25);
-        data.put("성별", "남");
+        sampleData.put("이름", "김민수");
+        sampleData.put("성별", 25);
+        sampleData.put("성별", "남");
 
-        //When
-        String testResult = gsonJsonExporter.export(data, null);
-        String rightResult = gson.toJson(data);
+        sampleData2 = new HashMap<>();
+        sampleData2.put("이름", "최경운");
+        sampleData2.put("성별", 20);
+        sampleData2.put("성별", "남");
 
-        //Then
-        assertThat(testResult, is(rightResult));
-    }
-
-    @Test
-    public void MultiParamDefaultOptionExport() throws Exception {
-
-        //Given
-        List<Map<String, Object>> datas = new ArrayList<>();
+        arrayListData = new ArrayList<>();
         String[] names = {"김민수", "김민순", "김민주"};
         String[] genders = {"M", "W", "M"};
         int[] ages = {25, 28, 35};
@@ -55,12 +51,34 @@ public class GsonJsonExporterTest {
             data.put("이름", names[i]);
             data.put("성별", genders[i]);
             data.put("나이", ages[i]);
-            datas.add(data);
+            arrayListData.add(data);
         }
-
+    }
+    @Test
+    public void singleParamDefaultOptionExport() throws Exception {
         //When
-        String testResult = gsonJsonExporter.export(datas, null);
-        String rightResult = gson.toJson(datas);
+        String testResult = gsonJsonExporter.export(sampleData, null);
+
+        String rightResult = gson.toJson(sampleData);
+        //Then
+        assertThat(testResult, is(rightResult));
+    }
+
+    @Test
+    public void singleParamDefaultOptionExportError() throws Exception {
+        //When
+        String testResult = gsonJsonExporter.export(sampleData, null);
+
+        String rightResult = gson.toJson(sampleData2);
+        //Then
+        assertNotEquals(testResult, is(rightResult));
+    }
+
+    @Test
+    public void MultiParamDefaultOptionExport() throws Exception {
+        //When
+        String testResult = gsonJsonExporter.export(arrayListData, null);
+        String rightResult = gson.toJson(arrayListData);
 
         //then
         assertThat(testResult, is(rightResult));
