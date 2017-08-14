0decf8c671a47287fb1bdb15abad9b2ce6bd44be
RohSunghyun <shtjdgus1090@naver.com>
2017-08-14 14:57:35 +0900

    마샬러 개발 완료
    마샬러 개발 완료

--- src/main/java/org/ahea/export/ExporterDummyMarshaller.java
+++ src/main/java/org/ahea/export/ExporterDummyMarshaller.java
@@ -3,6 +3,8 @@ package org.ahea.export;
 
 import org.ahea.build.entity.ResultData;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -15,11 +17,31 @@ public class ExporterDummyMarshaller {
         return new ExporterDummyMarshaller();
     }
 
+<<<<<<< Updated upstream
     public List<Map<String, Object>> marshalling(List<List<ResultData>> data) {
         //todo
         //marshaller 개발
+=======
+    public List<Map<String, Object>> marshalling(Map<String, List<ResultData>> data) {
+>>>>>>> Stashed changes
 
-        return null;
+        List<Map<String, Object>> result = new ArrayList<>();
+
+        data.forEach((s, resultDatas) -> {
+
+            Map fieldData = new HashMap();
+
+            resultDatas.forEach(resultData -> {
+                
+                fieldData.put(resultData.getName(),resultData.getValue());
+
+            });
+
+            result.add(fieldData);
+
+        });
+
+        return result;
     }
 
 
