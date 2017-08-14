6a10ad178aab03fcdca727c6167a887964de27af
RohSunghyun <shtjdgus1090@naver.com>
2017-08-14 23:04:25 +0900

    DataServiceFactory - applicationContext 적용
    DataServiceFactory - applicationContext 적용

--- src/main/java/org/ahea/build/service/DataServiceFactory.java
+++ src/main/java/org/ahea/build/service/DataServiceFactory.java
@@ -2,14 +2,42 @@ package org.ahea.build.service;
 
 import lombok.extern.apachecommons.CommonsLog;
 import org.ahea.build.entity.FieldCategory;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
 
 @CommonsLog
+@Component
 public class DataServiceFactory  {
 
+
+    public static ApplicationContext context;
+
+    @Autowired
+    public void setApplicationContext(ApplicationContext myContext)  {
+        context = myContext;
+    }
+
     public static GenetateDataInterface dateServiceCreate(FieldCategory fieldCategory){
+
+        GenetateDataInterface result = null;
+
         try {
-            return (GenetateDataInterface) fieldCategory.getCategoryType().getValue().newInstance();
-        } catch (InstantiationException | IllegalAccessException e) {
+
+            Class generateDataInterfaceClaz = fieldCategory.getCategoryType().getValue();
+
+            try {
+                Object generateDataInterfaceBeanObject = context.getBean(generateDataInterfaceClaz);
+                result = (GenetateDataInterface) generateDataInterfaceBeanObject;
+            } catch(NoSuchBeanDefinitionException noSuchBeanDefinitionException) {
+                result = (GenetateDataInterface) generateDataInterfaceClaz.newInstance();
+            }
+
+            return result;
+
+        }
+        catch (InstantiationException | IllegalAccessException e) {
             throw new RuntimeException(String.format("dateServiceCreate error-->%s",e.getMessage()));
         }
     }
