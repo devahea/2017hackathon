02e4182ed8d4548caee31dba1b05ecac24fd659c
ymkim <ymkim@digicaps.com>
2017-08-14 22:41:44 +0900

    build FilterSelect 수정

--- src/main/java/org/ahea/build/filter/FilterSelect.java
+++ src/main/java/org/ahea/build/filter/FilterSelect.java
@@ -82,8 +82,8 @@ public class FilterSelect {
     }
 
     private FilterManager addDateFilter(){
-
-        return null;
+        FilterManager filterManager = new FilterManager();
+        return filterManager;
     }
 
 }
